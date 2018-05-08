8bdaa064d95325988cdf4b35c00f0e6b0f4f33cb
Pawel Bus <pawelbus.dev@outlook.com>
2018-05-08 10:24:03 +0200

    Fix declaration Phone.prototype.printInfo

--- js/script.js
+++ js/script.js
@@ -4,11 +4,10 @@ function Phone(brand, price, color) {
 	this.color = color;
 }
 
-Phone.prototype.printInfo() {
+Phone.prototype.printInfo = function() {
 	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
 }
 
 var iPhone6S = new Phone('Apple', 2250, 'silver'),
 		Samsung Galaxy S6 = new Phone('Samsung Galaxy S6', 2300, 'white'),
 		OnePlus One = new Phone('OnePlus One', 1949, 'black');
-
