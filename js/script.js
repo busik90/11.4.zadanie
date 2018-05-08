9e111923852d5baf7b93daa0d10344d38bbc3119
Pawel Bus <pawelbus.dev@outlook.com>
2018-05-08 10:16:26 +0200

    Convert spaces to tabs

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
 function Phone(brand, price, color) {
-  this.brand = brand;
-  this.price = price;
-  this.color = color;
+	this.brand = brand;
+	this.price = price;
+	this.color = color;
 }
 
 Phone.prototype.printInfo() {
-  console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
+	console.log('The phone brand is ' + this.brand + ', color is ' + this.color + ' and the price is ' + this.price + '.');
 }
 
 var iPhone6S = new Phone('Apple', 2250, 'silver'),
-    Samsung Galaxy S6 = new Phone('Samsung Galaxy S6', 2300, 'white'),
-    OnePlus One = new Phone('OnePlus One', 1949, 'black');
+		Samsung Galaxy S6 = new Phone('Samsung Galaxy S6', 2300, 'white'),
+		OnePlus One = new Phone('OnePlus One', 1949, 'black');
+
